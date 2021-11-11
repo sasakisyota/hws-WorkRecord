e5659b48599ea42db5dcbeffe2916d1f8fecb85a
佐々木翔太 <sasakishouta@sasakishoutanoiMac.local>
2021-11-12 02:15:30 +0900

    add hws-WorkRecord

---
+++ hws-WorkRecord/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  hws-WorkRecord
+//
+//  Created by 佐々木翔太 on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+}
+
