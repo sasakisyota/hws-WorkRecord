844fb7c03cbf11abf48ae334f48b4cef24514bc5
佐々木翔太 <sasakishouta@sasakishoutanoiMac.local>
2021-11-13 02:14:23 +0900

    add xib

---
+++ hws-WorkRecord/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  hws-WorkRecord
+//
+//  Created by 佐々木翔太 on 2021/11/13.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
