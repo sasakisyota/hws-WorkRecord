95cff304d11b3c37a09fa80c5cd8eeb68daabc06
佐々木翔太 <sasakishouta@sasakishoutanoiMac.local>
2021-11-14 03:14:48 +0900

    add image

--- hws-WorkRecord/MainTableViewCell.swift
+++ hws-WorkRecord/MainTableViewCell.swift
@@ -1,23 +1,35 @@
-//
-//  MainTableViewCell.swift
-//  hws-WorkRecord
-//
-//  Created by 佐々木翔太 on 2021/11/13.
-//
+    //
+    //  MainTableViewCell.swift
+    //  hws-WorkRecord
+    //
+    //  Created by 佐々木翔太 on 2021/11/13.
+    //
 
-import UIKit
+    import UIKit
 
-class MainTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    class MainTableViewCell: UITableViewCell
+    {
+        @IBOutlet weak var label1: UILabel!
+        @IBOutlet weak var label2: UILabel!
+        @IBOutlet weak var img: UIImageView!
+        
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            // Initialization code
+        }
+        
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+            
+            // Configure the view for the selected state
+        }
+        
+        //画像・タイトル・説明文を設定するメソッド
+        func setCell(imageName: String, titleText: String, descriptionText: String){
+            img.image = UIImage(named: imageName)
+            label1.text = titleText
+            label2.text = descriptionText
+            
+        }
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-}
--- hws-WorkRecord/ViewController.swift
+++ hws-WorkRecord/ViewController.swift
@@ -7,10 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 3
+       }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
+        
+        cell.setCell(imageName: imageNames[indexPath.row], titleText: imageTitles[indexPath.row], descriptionText: imageDescriptions[indexPath.row])
+           return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let imageNames = ["ic_comfortable","ic_congestion","ic_normal"]
+
+    let imageTitles = ["快適","混雑","普通"]
+    
+    let imageDescriptions = ["作業日：2021年11月21日","作業日：2021年12月12日","作業日:2021年12月12日"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
     }
   
 }
